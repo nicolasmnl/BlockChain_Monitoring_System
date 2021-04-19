31deba5e5a2f47b9ed4d4caa4aa919bc87c64405
nicolasmnl <nicolas.leite@ccc.ufcg.edu>
2021-04-19 17:35:58 -0300

    AddBlock -- complete but still need fixes

--- supply_chain_blockchain/blockchain/models.py
+++ supply_chain_blockchain/blockchain/models.py
@@ -4,15 +4,29 @@ from hashlib import sha256
 # Create your models here.
 class Block(models.Model):
     objects = models.Manager()
-    index = models.AutoField(primary_key=False)
-    previous_hash = models.ForeignKey('self', on_delete=models.RESTRICT)
+    #Login n precisa adicionar(deve ser preenchido automatico)
+    index = models.IntegerField(primary_key=False)
+    #Login n precisa adicionar(deve ser preenchido automatico)
+    previous_hash = models.ForeignKey('self',blank=True ,null=True,on_delete=models.RESTRICT)
     current_transactions = models.TextField()
+    #Login n precisa adicionar(deve ser preenchido automatico)
     timestamp = models.DateTimeField(auto_now_add=True)
+    #Login n precisa adicionar(deve ser preenchido automatico)
     nonce = models.IntegerField()
+
     block_content = str(index) + str(previous_hash) + str(timestamp) + str(current_transactions) + str(nonce)
     first_hash = sha256(block_content.encode()).hexdigest()
-    hash = models.TextField(sha256(first_hash.encode()).hexdigest(), primary_key=True)
-
+    second_hash = sha256(first_hash.encode()).hexdigest()
+    hash = models.TextField(default=second_hash,primary_key=True)
 
     def __str__(self):
-        return str(self.__dict__)
+        return str(self.hash)
+
+
+    def calculate_hash(self, block_content):
+        first_hash = sha256(block_content.encode()).hexdigest()
+        second_hash = sha256(first_hash.encode()).hexdigest()
+        return second_hash
+
+
+#sha256(first_hash.encode()).hexdigest()
--- supply_chain_blockchain/blockchain/views.py
+++ supply_chain_blockchain/blockchain/views.py
@@ -11,9 +11,23 @@ def block_list(request):
 
 
 def block_create(request):
+    blocks = Block.objects.all()
     form = BlockForm(request.POST or None)
 
     if form.is_valid():
+        block = form.save(commit=False)
+        block.index = len(blocks)
+        new_hash = proof_of_work(block, block.current_transactions)
+        block.hash = new_hash
         form.save()
         return redirect('block_list')
     return render(request, 'block_form.html', {'form': form})
+
+def proof_of_work(block, current_transactions):
+    difficulty = 1
+    block.nonce = 0
+    computed_hash = block.hash
+    while not (computed_hash.endswith('0' * difficulty) and ('55' * difficulty) in computed_hash):
+        block.nonce += 1
+        computed_hash = block.calculate_hash(current_transactions)
+        return computed_hash
